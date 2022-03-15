ba9c46eb2b28a075effe388ccbeba1eca67dd7e0
NumiBlue <thekindlyblog@gmail.com>
2022-03-15 14:02:58 -0500

    error

--- __tests__/random.test.js
+++ __tests__/random.test.js
@@ -4,10 +4,3 @@ test('checks if 10 is equal to 10', () => {
   expect(checkIfEqual(10, 10)).toBe(true);
 });
 
-module.exports = function(val1, val2) {
-  if (val1 === val2) {
-    return true;
-  } else {
-    return false;
-  }
-};
